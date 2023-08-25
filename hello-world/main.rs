0b02880f3fbdc29ffe97469f4e554b264081ffc8
juacko <juacko18@gmail.com>
2023-08-25 02:34:11 -0500

    some more

--- hello-world/main.rs
+++ hello-world/main.rs
@@ -1,3 +1,23 @@
-fn main(){
-  println!("Hello world!");
+fn main() {
+
+  let mut a_number =10;
+  println!("The number is{}.", a_number);
+  let a_word = "Ten";
+
+  a_number = 15;
+  
+  println!("The number is {}.", a_number);
+  println!("The word is {}.", a_word);
+  
+  let shadow-number = 5;
+  let shadow-number = shadow-number + 5;
+  let shadow-number = shadow-number * 2;
+
+  println!("The shadow number is {}.", shadow-number);
+
+  let number: u32 = 14;
+  println!("The number is {}.", number);
 }
+
+
+
