ba5928f9ad4fb63addd4960ef72b9fe27748c119
juacko <juacko18@gmail.com>
2023-08-25 02:34:58 -0500

    commit 2

---
+++ hello-cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\juacko\Documents\code\rust-learning-path\hello-cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\juacko\Documents\code\rust-learning-path\hello-cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello-cargo/target/debug/hello-cargo.d
@@ -0,0 +1,1 @@
+C:\Users\juacko\Documents\code\rust-learning-path\hello-cargo\target\debug\hello-cargo.exe: C:\Users\juacko\Documents\code\rust-learning-path\hello-cargo\src\main.rs
